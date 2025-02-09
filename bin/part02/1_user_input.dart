0614523697cd059d3910ad8e07fd779a312ab1c9
Arafat <arafat2k30@gmail.com>
2025-02-10 03:57:41 +0600

    user input process add

---
+++ bin/part02/1_user_input.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+void main(){
+
+// Firstly, use stdout for entering value
+
+// String value? use stdin.readLineSync()
+
+// int value? use int.parse(stdin.readLineSync()!);
+//writeln use single line not anything is added
+//writeAll use for multiple line
+  print(' How can I do, need user input ');
+
+
+// For string value
+ // stdout.write('Enter your name : ');
+  //String? name= stdin.readLineSync();
+  //print('  My name is : ${name}');
+
+  // For int value
+
+  // stdout.write(' Enter a number ');
+  // int number= int.parse(stdin.readLineSync()!);
+  //print(number);
+
+  //stdout.writeln(' Hi all dart programmer ');
+ // String? name= stdin.readLineSync();
+  //print(name);
+
+  // stdout.writeAll([' Hi ',' Enter input', ' Boss']);
+  // String? name= stdin.readLineSync();
+  // print(name);
+
+
+
+
+}
