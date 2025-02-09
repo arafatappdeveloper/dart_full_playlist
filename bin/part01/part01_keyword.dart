d8675160ace75e8cf28161a2400585f89e93366b
Arafat <arafat2k30@gmail.com>
2025-02-10 00:19:00 +0600

    keyword

---
+++ bin/part01/part01_keyword.dart
@@ -0,0 +1,29 @@
+//
+// Keyword	Description
+// var	Declares a variable with inferred type.
+// final	Declares a variable that can be set only once (runtime constant).
+// const	Declares a compile-time constant.
+// int	Represents an integer data type.
+// double	Represents a floating-point number.
+// String	Represents a sequence of characters.
+// bool	Represents a boolean value (true or false).
+// dynamic	Represents a variable that can hold any type of value.
+// void	Indicates that a function does not return a value.
+// if, else	Used for conditional statements.
+// for, while, do	Used for loops.
+// switch, case, default	Used for multi-way branching.
+// class	Defines a class.
+// extends	Used for inheritance.
+// implements	Used to implement an interface.
+// abstract	Declares an abstract class or method.
+// async, await	Used for asynchronous programming.
+// import	Used to import libraries.
+// export	Used to export libraries.
+// part, part of	Used for splitting code into multiple files.
+// Contextual Keywords
+// These are not reserved words but have special meaning in specific contexts.
+//
+// Keyword	Description
+// on	Used in try-catch blocks and mixins.
+// show, hide	Used in import statements to include/exclude specific members.
+// sync, async*, sync*	Used in generator functions.
