0b08c31beb49afaa477e3a2110d693b8edc10f8f
Arafat <arafat2k30@gmail.com>
2025-02-10 00:45:45 +0600

    data type

---
+++ bin/part01/5_data_type.dart
@@ -0,0 +1,33 @@
+void main(){
+  // Basic data type are: int,double, boolean, string
+
+  //complex data type discuss here
+
+  //List : An ordered collection of items
+
+  List<String> language= ['Dart','Flutter', 'Java','C-programming','Javascript'];
+  List<int> number =[12,234,34,546,4567,57,65768];
+  List<double> result=[2.12,3.45,6.768];
+  List<bool> isbool=[true,false,false,true,true];
+  print(language);
+
+//Set : An unordered collection of unique items
+
+Set<String> name = {'Arafat, Zaman, Riad , Mithila'};
+print(name);
+
+
+
+//Map : A collection of key-value pairs
+
+Map<String ,int> student={
+  'Arafat': 1916043,
+  'Zaman': 1916044,
+  'Riad': 19160453,
+  'Mithila': 1916046,
+};
+
+print(student);
+
+
+}
