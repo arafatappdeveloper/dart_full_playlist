189d75f11c66f41bb5f64a1760de45cf34a253ab
Arafat <arafat2k30@gmail.com>
2025-02-09 23:41:40 +0600

    dart full playlist

---
+++ bin/dart_full_playlist.dart
@@ -0,0 +1,5 @@
+import 'package:dart_full_playlist/dart_full_playlist.dart' as dart_full_playlist;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_full_playlist.calculate()}!');
+}
---
+++ test/dart_full_playlist_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_full_playlist/dart_full_playlist.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
