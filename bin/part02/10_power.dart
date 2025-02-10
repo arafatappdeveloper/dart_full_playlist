6f877507c5c6ae46ecbb58610e5c277ed37b2782
Arafat <arafat2k30@gmail.com>
2025-02-10 13:08:32 +0600

    power added

---
+++ bin/part02/10_power.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+import 'package:io/io.dart';
+
+void main(){
+  num power=pow(5, 2);
+
+  num power1=pow(5, 3);
+  num power2=pow(6, 2);
+  num power3=pow(9, 3);
+
+  print(power);
+  print(power1);
+  print(power2);
+  print(power3);
+}
