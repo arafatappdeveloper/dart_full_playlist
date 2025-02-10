33c2ebc455aa671417c662fed752d68513e03882
Arafat <arafat2k30@gmail.com>
2025-02-10 13:29:42 +0600

    log,trigonmitc,exponentail

---
+++ bin/part02/11_log_trigonmiti.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+import 'package:io/io.dart';
+
+
+void main(){
+
+  double num= 10.5;
+  double x= 1;
+  
+  print(' Log value : ${log(num)}');
+  print(' Trigonmiti value : ${sin(x)} ,  ${cos(x)} , ${tan(x)} ');
+  print(' Exponential value : ${exp(num)}');
+}
