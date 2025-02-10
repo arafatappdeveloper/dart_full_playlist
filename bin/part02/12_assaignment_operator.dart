0ce7556c1ed786c1addfc870853d8221c93e73b3
Arafat <arafat2k30@gmail.com>
2025-02-10 17:45:55 +0600

    assignment operator

---
+++ bin/part02/12_assaignment_operator.dart
@@ -0,0 +1,32 @@
+import 'package:io/io.dart';
+
+void main(){
+
+  int a=10;
+  print(a);
+
+  int x=0;
+  double y=0;
+   x += 5;
+   print(x);
+  x -= 5;
+  print(x);
+  x *= 5;
+  print(x);
+
+  x ~/= 5;
+  print(x);
+  x &= 5;
+  print(x);
+  x ^= 5;
+  print(x);
+  x <<= 5;
+  print(x);
+  x >>= 5;
+  print(x);
+  y %= 5;
+  print(y);
+  y /= 5;
+  print(y);
+
+}
