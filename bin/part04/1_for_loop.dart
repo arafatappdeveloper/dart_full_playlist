a3acabd83b2095b0f139840c2afd0661bfad63cc
Arafat <arafat2k30@gmail.com>
2025-02-12 00:17:43 +0600

    for loop

---
+++ bin/part04/1_for_loop.dart
@@ -0,0 +1,10 @@
+void main(){
+  //The for loop is used when you know how many times you want to execute a block of code
+
+
+  for(int i=0;i<=10; i++){
+    print(' Hi Dart developer : $i');
+  }
+
+
+}
