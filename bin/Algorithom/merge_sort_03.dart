ff96d4666c4f08996e5dd87e0c8446317f186e89
Arafat <arafat2k30@gmail.com>
2025-04-15 03:22:56 +0600

    something change

--- bin/Algorithom/merge_sort_03.dart
+++ bin/Algorithom/merge_sort_03.dart
@@ -1,9 +1,10 @@
 import 'marge_sort_01.dart';
-
+int mergeCount = 0;
 void  main(){
   List<String> words = ["banana", "apple", "kiwi", "grape", "mango", "strawberry"];
   List<String> sortlist= mergeByLength(words);
    print(sortlist);
+  print("Total Merge Operations: $mergeCount ✅ for ");
 }
 
 List<String>mergeByLength(List<String>words){
@@ -21,21 +22,28 @@ List<String>mergeByLength(List<String>words){
 }
 
 List<String> mergeList(List<String> left, List<String>right){
+  mergeCount++;
+  print("Merging: $left + $right");
    List<String> result = [];
+
     int i= 0; int j = 0;
 
      while( i<left.length && j<right.length){
+
        if(left[i].length <=right[j].length){
          result.add(left[i]);
          i++;
+
        }else{
          result.add(right[j]);
          j++;
+
        }
      }
      result.addAll(left.sublist(i));
      result.addAll(right.sublist(j));
 
      return result ;
+
 
 }
