c4a38cffc5d425d899da0d0a8b2f5a51c2916d62
Arafat <arafat2k30@gmail.com>
2025-04-15 03:01:53 +0600

    sort by length

---
+++ bin/Algorithom/merge_sort_03.dart
@@ -0,0 +1,41 @@
+import 'marge_sort_01.dart';
+
+void  main(){
+  List<String> words = ["banana", "apple", "kiwi", "grape", "mango", "strawberry"];
+  List<String> sortlist= mergeByLength(words);
+   print(sortlist);
+}
+
+List<String>mergeByLength(List<String>words){
+  if(words.length<=1)return words;
+
+  int mid= words.length ~/ 2;
+  List<String> left = words.sublist(0,mid);
+  List<String> right = words.sublist(mid);
+
+   left = mergeByLength(left);
+   right = mergeByLength(right);
+
+   return mergeList(left,right);
+
+}
+
+List<String> mergeList(List<String> left, List<String>right){
+   List<String> result = [];
+    int i= 0; int j = 0;
+
+     while( i<left.length && j<right.length){
+       if(left[i].length <=right[j].length){
+         result.add(left[i]);
+         i++;
+       }else{
+         result.add(right[j]);
+         j++;
+       }
+     }
+     result.addAll(left.sublist(i));
+     result.addAll(right.sublist(j));
+
+     return result ;
+
+}
